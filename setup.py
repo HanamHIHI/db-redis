225e67a7a759ef9fe28595f061b24efb26654d02
Ubuntu <ubuntu@ip-172-31-92-242.ec2.internal>
2024-11-16 05:01:37 +0000

    for backup

--- setup.py
+++ setup.py
@@ -2,27 +2,26 @@
 Code samples for vector database quickstart pages:
     https://redis.io/docs/latest/develop/get-started/vector-database/
 """
-
-import json
-import time
-
 import numpy as np
 import pandas as pd
-import requests
 from redis.commands.json.path import Path
 import redis
 from redis.commands.search.field import (
     NumericField,
-    TagField,
     TextField,
     VectorField,
 )
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
-from redis.commands.search.query import Query
+
 client = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 df = pd.read_csv("df_final_v4.csv")
 df_mean_vectors = pd.read_csv("hanam_mean_vectors_100000.csv",header=None)
+df_category = pd.read_csv("category_embedding.csv")
+
+category_list = ['해물', '한식당', '돈가스', '양식', '고기', '카페', '식당', '일식당', '간식',
+       '햄버거', '까페', '식당 아님', '분식', '치킨', '호프', '피자', '일식집', '중국집', '베이커리',
+       '아시안 음식', '야채', '샤브샤브', '주류', '해장국', '디저트', '뷔페']
 
 print(len(df), len(df_mean_vectors))
 
@@ -43,6 +42,17 @@ schema = (
         },
         as_name="vector",
     ),
+    VectorField(
+        "$.category_vector",
+        "FLAT",
+        {
+            "TYPE": "FLOAT32",
+            "DIM": 768,
+            "DISTANCE_METRIC": "COSINE",
+        },
+        as_name="category_vector",
+    ),
+    
 )
 definition = IndexDefinition(prefix=["restaurant:"], index_type=IndexType.JSON)
 res = client.ft("idx:restaurant_vss").create_index(fields=schema, definition=definition)
@@ -55,35 +65,30 @@ for i in range(len(df)):
     vector = df_mean_vectors.iloc[i]
 
     if(pd.isna(row["name"]) != True):
-        temp_dict = {
-            "idx": int(row["index"]),
-            "name": row["name"],
-            "addr": str(row["position"]).strip('"').replace('~', ''),
-            "dist": int(row["total_distance"]),
-            "reqtime": int(row["total_time"]),
-            "category0": row["category3"],
-            "vector": vector.astype(np.float32).tolist(),
-        }
         if(pd.isna(row["category3"]) == True):
             temp_dict = {
-            "idx": int(row["index"]),
-            "name": row["name"],
-            "addr": str(row["position"]).strip('"'),
-            "dist": int(row["total_distance"]),
-            "reqtime": int(row["total_time"]),
-            "category0": '',
-            "vector": vector.astype(np.float32).tolist(),
-        }
+                "idx": int(row["index"]),
+                "name": row["name"],
+                "addr": str(row["position"]).strip('"'),
+                "dist": int(row["total_distance"]),
+                "reqtime": int(row["total_time"]),
+                "category0": '',
+                "vector": vector.astype(np.float32).tolist(),
+                "category_vector": df_category.iloc[category_list.index("식당 아님")].astype(np.float32).tolist(),
+            }
+        else:
+            temp_dict = {
+                "idx": int(row["index"]),
+                "name": row["name"],
+                "addr": str(row["position"]).strip('"').replace('~', ''),
+                "dist": int(row["total_distance"]),
+                "reqtime": int(row["total_time"]),
+                "category0": row["category3"],
+                "vector": vector.astype(np.float32).tolist(),
+                "category_vector": df_category.iloc[category_list.index(row["category3"])].astype(np.float32).tolist(),
+            }
     else:
-        temp_dict = {
-            "idx": int(row["index"]),
-            "name": '',
-            "addr": '',
-            "dist": -1,
-            "reqtime": -1,
-            "category0": '',
-            "vector": vector.astype(np.float32).tolist(),
-        }
+        continue
 
     # json_dict = json.dumps(temp_dict, ensure_ascii=False).encode('utf-8')
     # client.set(str(row["0"], json_dict))
--- test_search.py
+++ test_search.py
@@ -1,11 +1,16 @@
+import json
 from sentence_transformers import util
 from sentence_transformers import SentenceTransformer
 import torch
 import redis
 from redis.commands.search.query import Query
 
+import time
 import numpy as np
+import pandas as pd
 
+print("start setting")
+start = time.time()
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS', device=device) # pre-trained 모델 불러오기
@@ -14,17 +19,53 @@ model.eval() # gpu 너무 많이 잡아 먹어서 달아놓은 코드
 embeddings = model.encode(["팀점하기 좋은 식당"]).astype(np.float32).tolist()
 
 client = redis.Redis(host="localhost", port=6379, decode_responses=True)
+end = time.time()
+print(f"{end - start:.5f} sec")
 
-query = (
-    Query('(*)=>[KNN 3 @vector $query_vector AS vector_score]')
-     .sort_by('vector_score')
-     .return_fields('vector_score', 'id', 'brand', 'model', 'description')
+query0 = (
+    Query("*=>[KNN 1033 @vector $query_vector AS vector_score]")
+     .sort_by('vector_score', asc=False)
+     .return_fields('idx', 'vector_score')
      .dialect(2)
 )
-
-client.ft('idx:bikes_vss').search(
-    query,
+print("start0")
+start = time.time()
+res0 = client.ft('idx:restaurant_vss').search(
+    query0,
     {
       'query_vector': np.array(embeddings, dtype=np.float32).tobytes()
     }
 )
+end = time.time()
+print(f"{end - start:.5f} sec")
+
+query1 = (
+    Query("*=>[KNN 1033 @category_vector $query_category_vector AS category_vector_score]")
+     .sort_by('vector_score', asc=False)
+     .return_fields('idx', "category_vector_score")
+     .dialect(2)
+)
+print("start1")
+start = time.time()
+res1 = client.ft('idx:restaurant_vss').search(
+    query1,
+    {
+      'query_vector': np.array(embeddings, dtype=np.float32).tobytes()
+    }
+)
+end = time.time()
+print(f"{end - start:.5f} sec")
+
+print("start_sum")
+start = time.time()
+from collections import Counter
+result = dict(Counter(res0)+Counter(res1))
+
+df = pd.DataFrame.from_dict(data=result, orient='index').reset_index()
+df['2'] = df['0']+df['1']
+top5 = df.sort_values(by='2',ascending=False)[:5]
+
+df_data = pd.read_csv("df_final_v4.csv")
+df_final = pd.merge(df_data, top5, how="inner", on="index")
+end = time.time()
+print(f"{end - start:.5f} sec")
